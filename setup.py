0a1e94086fbb05a6b80437353e61f2347f84e2d6
Joos Kiener <joos.kiener@givaudan.com>
2020-11-20 09:18:13 +0100

    added setup.py and readme

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cxdml-styler",
+    version="0.0.1",    
+    author="Joos Kiener",
+    author_email="joos.kiener@gmail.com",
+    description="Change style of molecules in cdxml files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kienerj/cdxml-styler",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
+    ],
+    python_requires='>=3.6'
+)
