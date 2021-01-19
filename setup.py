4a6c0353d7304ab901a08322028f45cead5a16cd
Joos Kiener <joos.kiener@gmail.com>
2021-01-19 14:53:25 +0100

    bumped version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cdxml-slide-generator",
-    version="0.0.1",    
+    version="0.1.0",    
     author="Joos Kiener",
     author_email="joos.kiener@gmail.com",
     description="Generate cdxml documents for usage in PowerPoint",
