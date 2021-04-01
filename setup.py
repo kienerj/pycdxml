e4e43ed2491e65bce7c09e9c532087910fb5259a
Joos Kiener <joos.kiener@gmail.com>
2021-04-01 10:44:35 +0200

    moved build system to setup.cfg and pyproject.toml - added dependencies (except rdkit as that doesn't work with pip)

--- setup.py
+++ setup.py
@@ -1,24 +1,2 @@
 import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="chemdraw-tools",
-    version="0.1.0",    
-    author="Joos Kiener",
-    author_email="joos.kiener@gmail.com",
-    description="Work with and convert between ChemDraw Formats",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/kienerj/chemdraw-tools",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Development Status :: 4 - Beta"
-    ],
-    python_requires='>=3.6',
-    include_package_data=True
-)
+setuptools.setup()
