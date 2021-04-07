63a185222fbcb6fa08c41eaa8fe12e848e71bb97
Joos Kiener <joos.kiener@gmail.com>
2021-04-07 13:52:03 +0200

    code clean-up and added basic logging to cdxml_styler.py

--- pycdxml/cdxml_styler/cdxml_styler.py
+++ pycdxml/cdxml_styler/cdxml_styler.py
@@ -1,5 +1,8 @@
 import xml.etree.ElementTree as ET
 import numpy as np
+import logging
+
+logger = logging.getLogger('pycdxml.cdxml_styler')
 
 
 class CDXMLStyler(object):
@@ -8,7 +11,7 @@ class CDXMLStyler(object):
 <!DOCTYPE CDXML SYSTEM "http://www.cambridgesoft.com/xml/cdxml.dtd" >
 """
 
-    def __init__(self, style_name="ACS 1996", style_source=None, style_dict=None):
+    def __init__(self, style_name: str = "ACS 1996", style_source=None, style_dict: dict = None):
         """
         The output style can be defined by selecting one of the built in styles (ACS 1996 or Wiley), by
         specifying a path to a cdxml file that has the desired style or by supplying a dictionary containing the needed
@@ -35,31 +38,40 @@ class CDXMLStyler(object):
     
     def apply_style_to_file(self, cdxml_path, outpath=None):
         """
-        Converts the passed in cdxml to the defined style and writes the result to outpath. If outpath is none, the input
-        will be overwritten.
+        Converts the passed in cdxml to the defined style and writes the result to outpath. If outpath is none, the
+        input will be overwritten.
         :param cdxml_path: path of cdxml file to convert
         :param outpath: path to write converted file. If None will overwrite input file.
         :return:
         """
+        logger.debug("Applying style {} to file {}.".format(self.style, cdxml_path))
         tree = ET.parse(cdxml_path)
         root = tree.getroot()
         result = self._apply_style(root)
+        logger.debug("Style applied. Preparing for output.")
         xml = ET.tostring(result, encoding='unicode', method='xml')
         if outpath is None:
             outpath = cdxml_path
         with open(outpath, "w", encoding='UTF-8') as xf:
             file = f"{self.xml_header}{xml}"
             xf.write(file)
+        logger.debug("Style successfully applied and written output to file {}.".format(outpath))
 
-    def apply_style_to_string(self, cdxml):
+    def apply_style_to_string(self, cdxml: str) -> str:
+        """
+        Takes a cdxml as string, applies the style and returns a new cdxml as string.
 
+        :param cdxml: string containing cdxml data
+        :return: string containing cdxml with the desired style applied
+        """
+        logger.debug("Applying style {} to a cdxml string.".format(self.style))
         root = ET.fromstring(cdxml)
         result = self._apply_style(root)
-
         xml = ET.tostring(result, encoding='unicode', method='xml')
+        logger.debug("Style applied. Returning result cdxml string.")
         return self.xml_header + xml
 
-    def _apply_style(self, root) ->str:
+    def _apply_style(self, root: ET.Element) -> ET.Element:
 
         """Applies the selected style to the input cdxml string and all contained drawings and returns the modified
         cdxml as string.
@@ -67,7 +79,8 @@ class CDXMLStyler(object):
         Parameters:
         root (string): the root element of the cdxml
        """
-        # Set stlye on document level
+        # Set style on document level
+        logger.debug("Setting style on document level.")
 
         root.attrib["BondSpacing"] = self.style["BondSpacing"]
         root.attrib["BondLength"] = self.style["BondLength"]
@@ -83,28 +96,32 @@ class CDXMLStyler(object):
         bond_length = float(self.style["BondLength"])
 
         # Get all nodes (atoms) and bonds
+        logger.debug("Start applying style to molecules.")
         try:
             for fragment in root.iter('fragment'):
-
+                logger.debug("Applying style to fragment with id {}.".format(fragment.attrib["id"]))
                 CDXMLStyler.add_missing_bounding_box(fragment)
+                logger.debug("Getting coordinates and mapping.")
                 all_coords, node_id_mapping, bonds, label_coords = CDXMLStyler.get_coords_and_mapping(fragment)
 
                 num_nodes = len(node_id_mapping)
                 if num_nodes == 0:
                     raise ValueError("Molecule has no Atoms")
                 elif num_nodes == 1:
+                    logger.debug("Found single Node Fragment. Applying Label Style and returning.")
                     # only one node usually a text like 'HCl' -> only fix label size
                     for s in fragment.iter('s'):
                         s.attrib["size"] = self.style["LabelSize"]
                         s.attrib["face"] = self.style["LabelFace"]
                         s.attrib["font"] = self.style["LabelFont"]
                     return root
-
+                logger.debug("Calculating scaling.")
                 avg_bl = CDXMLStyler.get_avg_bl(all_coords, bonds, node_id_mapping)
 
                 # Scale Nodes (=Atoms)
                 scaling_factor = bond_length / avg_bl
                 scaled_coords = all_coords * scaling_factor
+                logger.debug("Determining new coordinates.")
                 final_coords = CDXMLStyler.translate(all_coords, scaled_coords)
                 # Scale atom labels
                 if len(label_coords) > 0:
@@ -113,6 +130,8 @@ class CDXMLStyler(object):
 
                 # bounding box of fragment
                 CDXMLStyler.fix_bounding_box(fragment, scaling_factor)
+
+                logger.debug("Applying new coordinates and label styles.")
 
                 node_attributes = ['id', 'p', 'Z', 'AS', 'Element', 'NumHydrogens', 'Geometry']
                 t_attributes = ['p', 'BoundingBox', 'LabelJustification', 'LabelAlignment']
@@ -124,17 +143,19 @@ class CDXMLStyler(object):
                     node.attrib['p'] = coords_xml
 
                     unwanted = set(node.attrib) - set(node_attributes)
-                    for unwanted_key in unwanted: del node.attrib[unwanted_key]
+                    for unwanted_key in unwanted:
+                        del node.attrib[unwanted_key]
 
                     for t in node.iter('t'):
                         if 'p' in t.attrib:
-                            #set new coordinates for lables (t elements)
+                            # set new coordinates for lables (t elements)
                             coords_label = str(final_labels[label_idx][0]) + " " + str(final_labels[label_idx][1])
                             t.attrib['p'] = coords_label
-                            label_idx +=1
+                            label_idx += 1
 
                         unwanted = set(t.attrib) - set(t_attributes)
-                        for unwanted_key in unwanted: del t.attrib[unwanted_key]
+                        for unwanted_key in unwanted:
+                            del t.attrib[unwanted_key]
 
                         for s in t.iter('s'):
                             s.attrib["size"] = self.style["LabelSize"]
@@ -150,9 +171,9 @@ class CDXMLStyler(object):
             raise ValueError("Molecule has no coordinates")
 
     @staticmethod
-    def add_missing_bounding_box(fragment):
+    def add_missing_bounding_box(fragment: ET.Element):
 
-        if not 'BoundingBox' in fragment.attrib:
+        if 'BoundingBox' not in fragment.attrib:
             all_coords = []
             for node in fragment.iter('n'):
                 if 'p' in node.attrib:
@@ -168,13 +189,12 @@ class CDXMLStyler(object):
             fragment.attrib['BoundingBox'] = "{} {} {} {}".format(min_x, min_y, max_x, max_y)
 
     @staticmethod
-    def get_coords_and_mapping(fragment):
+    def get_coords_and_mapping(fragment: ET.Element) -> tuple:
 
         bond_attributes = ['id', 'Z', 'B', 'E', 'BS', 'Order', 'BondCircularOrdering', 'Display']
 
         all_coords = []
         node_id_mapping = {}
-        bounding_boxes = {}
         label_coords = []
         label_bbs = []
         bonds = []
@@ -187,7 +207,7 @@ class CDXMLStyler(object):
             node_id_mapping[int(node.attrib['id'])] = idx
             for t in node.iter('t'):
                 if 'p' in t.attrib:
-                    label_p= [float(x) for x in t.attrib['p'].split(" ")]
+                    label_p = [float(x) for x in t.attrib['p'].split(" ")]
                     label_coords.append(label_p)
                     label_bb = [float(x) for x in t.attrib['BoundingBox'].split(" ")]
                     label_bbs.append(label_bb)
@@ -198,7 +218,8 @@ class CDXMLStyler(object):
             # Remove bond attributes set at bond level
             # Removing them will use the document level settings
             unwanted = set(bond.attrib) - set(bond_attributes)
-            for unwanted_key in unwanted: del bond.attrib[unwanted_key]
+            for unwanted_key in unwanted:
+                del bond.attrib[unwanted_key]
 
         all_coords = np.asarray(all_coords)
         label_coords = np.asarray(label_coords)
@@ -206,7 +227,7 @@ class CDXMLStyler(object):
         return all_coords, node_id_mapping, bonds, label_coords
 
     @staticmethod
-    def fix_bounding_box(element, scaling_factor):
+    def fix_bounding_box(element: ET.Element, scaling_factor: float):
 
         fragment_bb = np.asarray([float(x) for x in element.attrib['BoundingBox'].split(" ")])
         scaled_coords = fragment_bb * scaling_factor
@@ -221,14 +242,15 @@ class CDXMLStyler(object):
         translate = np.array([x_translate, y_translate, x_translate, y_translate])
         final_coords = scaled_coords + translate
         final_coords = np.round(final_coords, 2)
-        element.attrib['BoundingBox'] = "{} {} {} {}".format(final_coords[0], final_coords[1], final_coords[2], final_coords[3])
+        element.attrib['BoundingBox'] = "{} {} {} {}".format(
+            final_coords[0], final_coords[1], final_coords[2], final_coords[3])
 
     @staticmethod
-    def get_center(all_coords):
+    def get_center(all_coords: np.array) -> tuple:
         """Gets the center of current fragment
 
         Parameters:
-        all_coords (numpy): coordiantes of all nodes(atoms) of the fragment
+        all_coords (numpy): coordinates of all nodes(atoms) of the fragment
 
         Returns:
         tuple: (x,y) center point of fragment
@@ -244,11 +266,11 @@ class CDXMLStyler(object):
         return x_center, y_center
 
     @staticmethod
-    def get_avg_bl(all_coords, bonds, node_id_mapping):
+    def get_avg_bl(all_coords: dict, bonds: list, node_id_mapping: dict) -> float:
         """Gets the average bond length of current fragment
 
         Parameters:
-        all_coords (numpy): coordiantes of all nodes(atoms) of the fragment
+        all_coords (numpy): coordinates of all nodes(atoms) of the fragment
         bonds (list of dict): list of bonds where bond is a dict with start and end node id
         node_id_mapping (dict): maps node id to node idx
 
@@ -298,6 +320,7 @@ class CDXMLStyler(object):
     @staticmethod
     def get_style_from_cdxml(style_source):
 
+        logger.debug("Reading style from file {}.".format(style_source))
         tree = ET.parse(style_source)
         root = tree.getroot()
 
