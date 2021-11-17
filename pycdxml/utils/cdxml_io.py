b51f354b617815faa99aef9d29adc973bb7b1dc2
Joos Kiener <joos.kiener@gmail.com>
2021-11-17 15:52:09 +0100

    setting doctype correctly when converting etree to string

--- pycdxml/utils/cdxml_io.py
+++ pycdxml/utils/cdxml_io.py
@@ -2,7 +2,6 @@ from lxml import etree as ET
 import re
 
 CDXML_HEADER = """<?xml version="1.0" encoding="UTF-8" ?>
-<!DOCTYPE CDXML SYSTEM "http://www.cambridgesoft.com/xml/cdxml.dtd" >
 """
 
 
@@ -11,7 +10,8 @@ def etree_to_cdxml(xml: ET) -> str:
     Creates a cdxml string from the given lxml ElementTree instance with the correct xml headers.
     ChemDraw requires these headers to be set exactly like this or else the file is not recognized as cdxml
     """
-    xml = ET.tostring(xml, encoding='unicode', method='xml')
+    xml = ET.tostring(xml, encoding='unicode', method='xml',
+                      doctype="<!DOCTYPE CDXML SYSTEM \"http://www.cambridgesoft.com/xml/cdxml.dtd\" >")
     return CDXML_HEADER + xml
 
 
