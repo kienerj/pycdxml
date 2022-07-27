196993f4a61823d29fc13542c88219e3c1ef52d0
Joos Kiener <joos.kiener@gmail.com>
2022-07-27 10:51:26 +0200

    more clean-up and using method from geometry module in cdxml_styler

--- pycdxml/cdxml_styler/cdxml_styler.py
+++ pycdxml/cdxml_styler/cdxml_styler.py
@@ -142,14 +142,14 @@ class CDXMLStyler(object):
                 scaling_factor = bond_length / avg_bl
                 scaled_coords = all_coords * scaling_factor
                 logger.debug("Determining new coordinates.")
-                x_translate, y_translate = CDXMLStyler.get_translation_coordinates(all_coords, scaled_coords)
-                final_coords = CDXMLStyler.translate(scaled_coords, x_translate, y_translate)
+                x_translate, y_translate = geometry.get_translation(all_coords, scaled_coords)
+                final_coords = geometry.translate(scaled_coords, x_translate, y_translate)
                 # Scale atom labels
                 if len(label_coords) > 0:
                     scaled_labels = label_coords * scaling_factor
                     x_translate_label, y_translate_label = \
-                        CDXMLStyler.get_translation_coordinates(label_coords, scaled_labels)
-                    final_labels = CDXMLStyler.translate(scaled_labels, x_translate_label, y_translate_label)
+                        geometry.get_translation(label_coords, scaled_labels)
+                    final_labels = geometry.translate(scaled_labels, x_translate_label, y_translate_label)
 
                 # bounding box of fragment
                 geometry.fix_bounding_box(fragment, x_translate, y_translate, scaling_factor)
@@ -298,26 +298,6 @@ class CDXMLStyler(object):
         return all_coords, node_id_mapping, bonds, label_coords
 
     @staticmethod
-    def get_center(all_coords: np.array) -> tuple:
-        """Gets the center of current fragment
-
-        Parameters:
-        all_coords (numpy): coordinates of all nodes(atoms) of the fragment
-
-        Returns:
-        tuple: (x,y) center point of fragment
-
-       """
-
-        max_x, max_y = all_coords.max(axis=0)
-        min_x, min_y = all_coords.min(axis=0)
-
-        x_center = (min_x + max_x) / 2
-        y_center = (min_y + max_y) / 2
-
-        return x_center, y_center
-
-    @staticmethod
     def get_avg_bl(all_coords: dict, bonds: list, node_id_mapping: dict) -> float:
         """Gets the average bond length of current fragment
 
@@ -345,44 +325,6 @@ class CDXMLStyler(object):
         bond_length = np.linalg.norm(a - b, axis=1)  # thanks to stackoverflow
         avg_bl = round(np.mean(bond_length), 1)
         return avg_bl
-
-    @staticmethod
-    def get_translation_coordinates(all_coords, scaled_coords):
-        """Gets the x and y translation needed to scale the fragment back to it's previous center
-
-        Parameters:
-        all_coords (numpy): coordinates of all nodes(atoms) of the fragment
-        scaled_coords(numpy): coordinates of all nodes(atoms) of the fragment after scaling
-
-        Returns:
-        tuple: x and y amount to translate
-        """
-
-        x_center, y_center = CDXMLStyler.get_center(all_coords)
-        scaled_x_center, scaled_y_center = CDXMLStyler.get_center(scaled_coords)
-
-        x_translate = x_center - scaled_x_center
-        y_translate = y_center - scaled_y_center
-
-        return x_translate, y_translate
-
-    @staticmethod
-    def translate(coords, x_translate, y_translate):
-        """Translates the input coordinates by the given x and y translation amount
-
-        Parameters:
-        coords (numpy): coordinates of all elements to translate
-        x_translate: amount to translate on x-axis
-        y_translate: amount to translate on y-axis
-
-
-        Returns:
-        numpy: array of translated coordinates
-
-       """
-        translate = np.array([x_translate, y_translate])
-        final_coords = coords + translate
-        return final_coords
 
     @staticmethod
     def get_style(style_name):
